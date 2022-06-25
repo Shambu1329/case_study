b4f1748702a785d7f6b19c26c539be24a1a17060
Shambu1329 <shambukrishna34@gmail.com>
2022-06-25 12:34:27 +0530

    complete

---
+++ contact.js
@@ -0,0 +1,18 @@
+contactbtnevent=()=>{
+    Name=(document.getElementById("Name").value)
+    Message=(document.getElementById("Message").value)
+    Phone=(document.getElementById("Phone").value)
+
+
+
+    console.log(Name)
+    alert(Name)
+
+    console.log(Message)
+    alert(Message)
+
+
+    console.log(Phone)
+    alert(Phone)
+    
+}
